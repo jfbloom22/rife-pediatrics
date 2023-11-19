64fe8cb7b188c890728a38a2ea178b11e6d5cd0b
Jonathan Flower <jfbloom22@gmail.com>
2023-11-18 20:56:32 -0500

    updated forms to formspree

---
+++ node_modules/.pnpm/@builder.io+sdk-svelte@0.7.0_svelte@5.0.0-next.7/node_modules/@builder.io/sdk-svelte/lib/browser/functions/evaluate/placeholder-runtime.d.ts
@@ -0,0 +1,2 @@
+import type { ExecutorArgs } from './helpers.js';
+export declare const evaluator: (_args: ExecutorArgs) => never;
---
+++ node_modules/.pnpm/@builder.io+sdk-svelte@0.7.0_svelte@5.0.0-next.7/node_modules/@builder.io/sdk-svelte/lib/browser/helpers/preview-lru-cache/get.d.ts
@@ -0,0 +1,2 @@
+import type { BuilderContent } from '../../types/builder-content.js';
+export declare function getPreviewContent(_searchParams: URLSearchParams): BuilderContent | undefined;
